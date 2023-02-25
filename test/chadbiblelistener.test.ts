0b9c8adfea4ad4a9ff0a5543b7cf80bb903cfc1f
gin7018 <79063763+gin7018@users.noreply.github.com>
2023-02-24 19:31:15 -0500

    Initial commit

---
+++ test/chadbiblelistener.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Chadbiblelistener from '../lib/chadbiblelistener-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chadbiblelistener-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Chadbiblelistener.ChadbiblelistenerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
