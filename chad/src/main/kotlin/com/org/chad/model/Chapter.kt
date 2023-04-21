21af985371380519b400a7c3ff430f639abdfd0e
gin7018 <79063763+gin7018@users.noreply.github.com>
2023-04-21 14:04:19 -0400

    making chapter pretty

--- chad/src/main/kotlin/com/org/chad/model/Chapter.kt
+++ chad/src/main/kotlin/com/org/chad/model/Chapter.kt
@@ -6,4 +6,22 @@ data class Chapter (
     @JsonProperty("bookName") val bookName: String,
     @JsonProperty("chapterNumber") val chapterNumber: Int,
     @JsonProperty("file") val file: ByteArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Chapter
+
+        if (bookName != other.bookName) return false
+        if (chapterNumber != other.chapterNumber) return false
+        return file.contentEquals(other.file)
+    }
+
+    override fun hashCode(): Int {
+        var result = bookName.hashCode()
+        result = 31 * result + chapterNumber
+        result = 31 * result + file.contentHashCode()
+        return result
+    }
+}
